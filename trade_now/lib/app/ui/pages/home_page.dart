92ae3e0d4ccbb1b910f4ac4c32a52320369250a6
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2024-09-04 20:28:30 -0300

    add: initial structure of homepage

--- trade_now/lib/app/ui/pages/home_page.dart
+++ trade_now/lib/app/ui/pages/home_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:trade_now/app/controllers/home_controller.dart';
+import 'package:trade_now/app/core/constants/color_constants.dart';
+import 'package:trade_now/app/ui/components/navigation_bar.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
+  HomePage({super.key});
+  final controller = Get.put(HomeController());
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const SearchBar(
+          hintText: "Busque seu produto",
+        ),
+        actions: const [Icon(Icons.notifications)],
+        backgroundColor: darkerColor,
+      ),
+      body: const Placeholder(),
+      bottomNavigationBar: const NavBar(),
+      backgroundColor: bgColor,
+    );
   }
 }
