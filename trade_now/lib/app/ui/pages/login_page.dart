d45b473d5c23bb28ab236caea0f292000b980bc8
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2024-09-04 20:28:50 -0300

    add: initial structure of loginpage

--- trade_now/lib/app/ui/pages/login_page.dart
+++ trade_now/lib/app/ui/pages/login_page.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:trade_now/app/core/constants/color_constants.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Trade Now",
+          style: TextStyle(color: lightColor),
+        ),
+        centerTitle: true,
+        toolbarHeight: 80,
+        backgroundColor: darkerColor,
+      ),
+      body: Form(
+          child: SizedBox(
+        width: 400,
+        height: 300,
+        child: Column(
+          children: [
+            Column(
+              children: [const Text("Email"), TextFormField()],
+            ),
+            Column(
+              children: [
+                const Text("Senha"),
+                TextFormField(),
+              ],
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text("Login"))
+          ],
+        ),
+      )),
+      backgroundColor: bgColor,
+    );
   }
 }
