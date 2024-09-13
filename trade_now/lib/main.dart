b663dde915650ebc0bcdf1b004488f8dd59fbfbf
Caio Tavares <102376017+Caio-T4vares@users.noreply.github.com>
2024-09-13 20:07:22 -0300

    Adicionado o firebase ao projeto.

--- trade_now/lib/main.dart
+++ trade_now/lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:trade_now/app/routes/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const TradeNowApp());
 }
 
